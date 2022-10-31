23ddc32de811717858d43313a3f98cf4758369c7
Devika Jain <devika.jain18@gmail.com>
2022-10-31 14:54:09 +0530

    modified temlates and changed the DB to postgres

--- mysite01/settings.py
+++ mysite01/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_extensions'
+    'django_extensions',
+    'rest_framework'
+
 ]
 
 MIDDLEWARE = [
@@ -78,8 +80,11 @@ WSGI_APPLICATION = 'mysite01.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'letsexplore',
+        'USER':'postgres',
+        'PASSWORD':'postgres',
+        'HOST':'localhost'
     }
 }
 
